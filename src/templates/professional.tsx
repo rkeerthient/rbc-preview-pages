75dd260105c9cee6ef30ab93289aed8d61f7f8cd
rkeerthient <rkeerthi@yext.com>
2024-01-30 02:39:58 +0530

    updated

--- src/templates/professional.tsx
+++ src/templates/professional.tsx
@@ -167,6 +167,7 @@ export const config: TemplateConfig = {
     // Defines the scope of entities that qualify for this stream.
     filter: {
       entityTypes: ["financialProfessional"],
+      savedFilterIds: ["1379282530"],
     },
     // The entity language profiles that documents will be generated for.
     localization: {
